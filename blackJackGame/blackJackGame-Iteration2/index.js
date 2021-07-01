c7785eadfe521201040eb2b123620b6e671d07d8
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-01 18:23:16 +0530

    render game function

--- blackJackGame/blackJackGame-Iteration2/index.js
+++ blackJackGame/blackJackGame-Iteration2/index.js
@@ -4,6 +4,7 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let cards = [firstCard , secondCard] //9
 
 // 2. Create a startGame() function. Move the conditional
 // below (line 11-20) inside the body of the function.
@@ -16,14 +17,26 @@ let message = ""
 
 //6. New card button : Create new card function
 
+// 7. Add to the sum when newCard is clicked
+
+//8. Rename startgame to renderGame and let startGame invoke renderGame
+
+//9 . Solving cards problem using an array
+
+
+
 let messageEl = document.getElementById("message-el") //3
 let sumEl = document.getElementById("sum-el") //4
 let cardsEl = document.getElementById("cards-el") //5
 
-function startGame() {
+function startGame () {
+    renderGame()
+}
+
+function renderGame() {
 
     sumEl.textContent = "Sum:" + sum //4
-    cardsEl.textContent = "Cards:" + " " + firstCard + " - " + secondCard //5
+    cardsEl.textContent = "Cards:" + " " + cards[0] + " - " + cards[1] //5 , 9
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
@@ -37,5 +50,15 @@ function startGame() {
 
     messageEl.textContent = message //3
     
+}
+
+function newCard() {                        //6
+    console.log(" Drwaing a new Card");
+
+    let card = 5 //7
+
+    sum += card //7
+
+    renderGame()  //7
 
 }
